f8a6f7ad58c3e940f9f332bcded726912bb26466
Alan <wang_my00@outlook.com>
2022-11-11 14:16:20 -0800

    add cleaning code

---
+++ data/data_cleaning.sql
@@ -0,0 +1,16 @@
+select 
+    strftime('%m', FL_DATE) as 'month', 
+    strftime('%w', FL_DATE) as day_of_week, 
+    OP_CARRIER as airline, 
+    origin as 'origin',
+    dest as 'dest',
+    count(*) as num_of_flights,
+    avg(arr_delay) as avg_delay
+from 
+    airline_2009
+group by
+    strftime('%m', FL_DATE), 
+    strftime('%w', FL_DATE), 
+    OP_CARRIER, 
+    origin, 
+    dest
