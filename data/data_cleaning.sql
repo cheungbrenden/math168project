eb0144912be431ce3b53cf9aef015039f333ea73
Alan <wang_my00@outlook.com>
2022-11-11 14:17:57 -0800

    minor

--- data/data_cleaning.sql
+++ data/data_cleaning.sql
@@ -1,12 +1,12 @@
-select 
+select
     strftime('%m', FL_DATE) as 'month', 
     strftime('%w', FL_DATE) as day_of_week, 
     OP_CARRIER as airline, 
-    origin as 'origin',
-    dest as 'dest',
+    origin,
+    dest,
     count(*) as num_of_flights,
     avg(arr_delay) as avg_delay
-from 
+from
     airline_2009
 group by
     strftime('%m', FL_DATE), 
