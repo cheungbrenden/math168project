cbd7460336c5d7077d522ae8b0bfaf9a36aab5f8
Alan <wang_my00@outlook.com>
2022-11-20 01:35:36 -0800

    a

--- metrics.py
+++ metrics.py
@@ -1,9 +1,11 @@
 from util import *
 
 
-# Metrics
+# 1.b Assortativity
 def gini_coef(G):
     deg_dist = np.array(list(dict(G.degree(weight="num_of_flights")).values()))
     res = 0
     for i in deg_dist:
         res += np.abs(deg_dist - i).sum()
+    return res / G.number_of_nodes() / deg_dist.sum()
+
