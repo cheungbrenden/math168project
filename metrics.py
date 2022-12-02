6497cec05235305ed11690e5ec1dd89894b1de96
Alan <wang_my00@outlook.com>
2022-12-02 00:13:10 -0800

    add data for all years

--- metrics.py
+++ metrics.py
@@ -129,16 +129,7 @@ def average_shortest_path_length(G):
 
 
 # 2.a resilience of a given airport
+# Note: it can take up to 6 minutes to compute this value
 def resilience(G):
     # 1. Relative strength s_i
-    s_i = np.array([])
-    for i in G.nodes():
-        temp = 0
-        for j in G.neighbors(i):
-            temp += G.get_edge_data(i, j, 'num_of_flights')['num_of_flights']
-        s_i = np.append(s_i, temp)
-    s_i_hat = s_i / s_i.sum()
-
-    s_j_sum = s_i.sum()
-    v_i = -2 * s_i + s_j_sum
-    # R_i =
+    return nx.average_node_connectivity(G)
