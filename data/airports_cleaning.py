7f014558d1e62c5953f1a9f5fca8ecc79615dafc
Alan <wang_my00@outlook.com>
2022-12-02 23:09:56 -0800

    graphs

--- data/airports_cleaning.py
+++ data/airports_cleaning.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 airport = pd.read_csv("airports.dat", index_col=0, header=None)
-airport = airport[(airport[3] == "United States") | (airport[3] == "Puerto Rico") | (airport[3] == "Virgin Islands")]
+airport = airport[(airport[3] == "United States") | (airport[3] == "Puerto Rico") | (
+    airport[3] == "Virgin Islands") | (airport[3] == "Guam")]
 airport = airport[airport[4] != "\\N"][[4, 6, 7]]
 airport.columns = ["airport", "latitude", "longitude"]
 airport.set_index("airport").to_csv("airports_us.csv")
--- util.py
+++ util.py
@@ -3,6 +3,7 @@ import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
+import matplotlib.lines as mlines
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -17,6 +18,7 @@ def create_graph(data):
     Returns:
         a directed, weighted graph, with weight with the name "num_of_flights"
     """
+    data.columns = [i.lower() for i in data.columns]
     temp = data.groupby(["origin", "dest"]).sum().reset_index()
     return nx.from_pandas_edgelist(temp, source='origin', target='dest', edge_attr=['num_of_flights'], create_using=nx.DiGraph)
 
@@ -28,7 +30,7 @@ m = Basemap(
         llcrnrlon=-180,
         llcrnrlat=10,
         urcrnrlon=-50,
-        urcrnrlat=70,
+        urcrnrlat=65,
         lat_ts=0,
         resolution='l',
         suppress_ticks=True)
@@ -45,7 +47,8 @@ def viz_map(flights, year):
         flights: a cleaned dataframe 
         year: just input a year, so it can make the right title and save with the correct file name
     """
-    
+    flights.columns = [i.lower() for i in flights.columns]
+
     plt.figure(figsize = (10,9))
     temp = flights.groupby(["origin", "dest"]).sum().reset_index()
     counts = pd.DataFrame(temp.origin.value_counts())
@@ -53,28 +56,32 @@ def viz_map(flights, year):
     G_2009 = nx.from_pandas_edgelist(temp, source='origin', target='dest', edge_attr=['num_of_flights'], create_using=nx.DiGraph)
     plt.figure(figsize=(15,20))
     nx.draw_networkx_nodes(G = G_2009, pos = pos, nodelist = [x for x in G_2009.nodes() if counts['total_flight'][x] >= 100],
-                                node_color = 'r', alpha = 0.8,
+                           node_color='#e74c3c', alpha=0.8,
                                 node_size = [counts['total_flight'][x]*4  for x in G_2009.nodes() if counts['total_flight'][x] >= 100])
 
     nx.draw_networkx_labels(G = G_2009, pos = pos, font_size=10,
                             labels = {x:x for x in G_2009.nodes() if counts['total_flight'][x] >= 100})
 
     nx.draw_networkx_nodes(G = G_2009, pos = pos, nodelist = [x for x in G_2009.nodes() if counts['total_flight'][x] < 100],
-                            node_color = 'b', alpha = 0.6,
+                           node_color='#f7a503', alpha=0.6,
                             node_size = [counts['total_flight'][x]*4  for x in G_2009.nodes() if counts['total_flight'][x] < 100])
 
-    nx.draw_networkx_edges(G = G_2009, pos = pos, edge_color = 'g', width = temp['num_of_flights']*0.00081, 
+    nx.draw_networkx_edges(G=G_2009, pos=pos, edge_color='#2D68C4', width=temp['num_of_flights']*0.00081,
                             alpha=0.1, arrows = False)
 
     m.drawcountries(linewidth = 3)
     m.drawstates(linewidth = 0.2)
     m.drawcoastlines(linewidth=1)
     m.fillcontinents(alpha = 0.3)
-    line1 = mlines.Line2D(range(1), range(1), color="white", marker='o', markerfacecolor="red")
-    line2 = mlines.Line2D(range(1), range(1), color="white", marker='o',markerfacecolor="blue")
-    line3 = mlines.Line2D(range(1), range(1), color="green", marker='',markerfacecolor="green")
+    line1 = mlines.Line2D(range(1), range(1), color="white",
+                          marker='o', markerfacecolor="#e74c3c")
+    line2 = mlines.Line2D(range(1), range(1), color="white",
+                          marker='o', markerfacecolor="#f7a503")
+    line3 = mlines.Line2D(range(1), range(1), color="#2D68C4",
+                          marker='', markerfacecolor="#2D68C4")
     plt.legend((line1, line2, line3), ('Large Airport >= 100 routes', 'Smaller airports', 'routes'))
     plt.title(f"Network graph of flight routes in the USA in {year}", fontsize = 30)
     plt.tight_layout()
-    plt.savefig(f"./images/{year}.jpg", format = "jpg", dpi = 300)
+    plt.savefig(f"./images/{year}.jpg", format="jpg",
+                dpi=300, bbox_inches="tight")
     plt.show()
